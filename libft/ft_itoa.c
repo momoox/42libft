212e04dabf529e1229df03c116927d498ace16e1
Momox <119704058+momoox@users.noreply.github.com>
2022-12-16 18:21:47 +0100

    Add files via upload

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,42 +6,64 @@
 /*   By: mgeisler <mgeisler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 17:15:36 by mgeisler          #+#    #+#             */
-/*   Updated: 2022/12/15 18:54:02 by mgeisler         ###   ########.fr       */
+/*   Updated: 2022/12/16 16:36:38 by mgeisler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	len(int n)
+int	len(long int n)
 {
 	int	i;
 
 	i = 0;
+	if (n < 0)
+		i++;
+	if (n == 0)
+		return (1);
 	while (n != 0)
 	{
+		n = n / 10;
 		i++;
-		n--;
 	}
 	return (i);
+}
+
+long int	secu(long int nb, char **str)
+{
+	if (nb < 0)
+	{
+		(*str)[0] = '-';
+		nb *= -1;
+	}
+	if (nb == 0)
+	{
+		(*str)[0] = '0';
+		(*str)[1] = '\0';
+		return (nb);
+	}
+	return (nb);
 }
 
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		nb;
 	int		i;
+	long	nb;
 
-	nb = 0;
-	i = 0;
-	str = malloc(sizeof(char) * len(n) + 1);
+	nb = n;
+	i = len(nb);
+	str = malloc(sizeof(char) * i + 1);
 	if (!str)
-		return(NULL);
-	if (n < 0)
+		return (NULL);
+	nb = secu(nb, &str);
+	str[i] = '\0';
+	i--;
+	while (nb)
 	{
-		nb *= -1;
-		str[0] = '-';
+		str[i] = '0' + (nb % 10);
+		nb = nb / 10;
+		i--;
 	}
-	while (n != 0)
-		str[i] = n;
 	return (str);
 }
